bb6a11538a633f360560f110081c3d6aa17730c1
svivek19 <svivek030503@gmail.com>
2024-03-30 12:48:46 +0530

    header finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <h1>GitHub Profile Finder</h1>
-      <button>Demo</button>
+    <div className="w-5/6 mx-auto flex justify-between bg-slate-900 text-white p-5 m-5 rounded-md">
+      <h1 className="text-2xl font-semibold">GitHub Profile Finder</h1>
+      <button className="text-xl font-semibold">Demo</button>
     </div>
   );
 }
