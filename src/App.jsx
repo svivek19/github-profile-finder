3bbfa03720e6349f1d1366c9866b18d9202cc0f2
svivek19 <svivek030503@gmail.com>
2024-03-30 13:03:44 +0530

    input box finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import InputElement from "./components/InputElement";
 
 function App() {
   return (
     <>
       <Header />
+      <InputElement />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header() {
+function Header() {
   return (
     <div className="w-5/6 mx-auto flex justify-between bg-slate-900 text-white p-5 m-5 rounded-md">
       <h1 className="text-2xl font-semibold">GitHub Profile Finder</h1>
@@ -8,3 +8,5 @@ export default function Header() {
     </div>
   );
 }
+
+export default Header;
